d5dd8728e2f1aca3fda9e9098b2e485a73b10612
lazytigertech <info2691881@gmail.com>
2025-03-23 15:09:14 +0100

    Add files via upload

---
+++ design/login_page_blue.js
@@ -0,0 +1,8 @@
+function openDocsSideNav(){document.getElementById("docs-side-nav").style.width="100%";document.getElementById("docs-side-nav").style.paddingLeft="24px";document.body.style.overflow="hidden";}
+function closeDocsSideNav(){document.getElementById("docs-side-nav").style.width="0";document.getElementById("docs-side-nav").style.paddingLeft="0px";document.body.style.overflow="auto";}
+
+document.getElementById("login-button-dropdown").style.display = "none";
+
+function openLogin(){
+    console.log("login");
+}
